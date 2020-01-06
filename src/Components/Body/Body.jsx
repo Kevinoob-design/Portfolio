587b7221a67d52122e412cced79a9c1114594ec0
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-06 01:52:00 -0400

    Finished personal experience section with JSON model

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -11,6 +11,7 @@ function Body(){
     <div>
         <About/>
         <WorkExp/>
+            <PerpExp/>
     </div>);
 }
 
--- src/Components/Body/PerExp.jsx
+++ src/Components/Body/PerExp.jsx
@@ -1,9 +1,20 @@
 //jshint esversion:6 
 
 import React from "react";
+import PerExpCard from "./BodyComponents/PerExpCardComponent";
+import perExpModel from "./Model/PerExp.json";
 
 function PerExp(){
+    return (
+        <div>
+            <div className="pb-5 md:max-w-6xl mx-auto">
+                <div className="md:w-1/2 w-auto text-3xl text-center border-4 mx-auto">PERSONAL EXPERIENCE</div>
 
+                {perExpModel.map(element => {
+                    return (<PerExpCard params={element}/>);
+                })}
+            </div>
+        </div>
+    )
 }
-
 export default PerExp;
