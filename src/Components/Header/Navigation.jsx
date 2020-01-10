7b2048e13926b58279001db28862992adf000831
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-09 23:38:30 -0400

    Added animated hamburger for the menu and change translation of the fadeInOut of menu list

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,10 +1,11 @@
 //jshint esversion:6
 
 import React, { useState } from "react";
-import ListIcon from '@material-ui/icons/List';
 
 function Nav(){
     const [menuCollapse, setMenuCollapse] = useState("");
+
+    // const [hamburger, setHamburgerCollapse] = (" ");
 
     const [animate, setAnimation] = useState(() => {
         if (window.innerWidth < 768) {
@@ -25,6 +26,7 @@ function Nav(){
     function handleMenuCollapse() {
 
         document.getElementById("body").classList.toggle("overflow-hidden");
+        document.getElementById("ham").classList.toggle("is-active");
 
         setMenuCollapse(prev => {
             if (prev === "menuCollapse") {     
@@ -41,18 +43,21 @@ function Nav(){
 
     return (
         <nav className={"top-0 p-1 fixed bg-gray-900 md:m-auto w-full md:h-auto md:relative z-50 " + menuCollapse}>
-
-            <div className="inline-block md:hidden">
-                <button onClick={handleMenuCollapse} className="border-gray-900 focus:outline-none ml-2 block">
-                    <ListIcon fontSize="large" /></button>
+            
+            <div className="inline-block md:hidden pt-4">
+                <button id="ham" onClick={handleMenuCollapse} className="border-gray-900 focus:outline-none ml-2 block hamburger hamburger--collapse-r">
+                    <span className="hamburger-box">
+                        <span className="hamburger-inner"></span>
+                    </span>
+                </button>
             </div>
 
             <ul className={elementHidden + " md:flex w-screen md:w-auto justify-center"}>
-                <li className={animate + " text-2xl p-5"}><a href="#">Home</a></li>
-                <li className={animate + " text-2xl p-5"}><a href="#">About</a></li>
-                <li className={animate + " text-2xl p-5"}><a href="#">Work</a></li>
-                <li className={animate + " text-2xl p-5"}><a href="#">Experience</a></li>
-                <li className={animate + " text-2xl p-5"}><a href="#">Contact</a></li>
+                <li className={animate + " text-2xl p-5 fast"}><a href="#">Home</a></li>
+                <li className={animate + " text-2xl p-5 fast"}><a href="#">About</a></li>
+                <li className={animate + " text-2xl p-5 fast"}><a href="#">Work</a></li>
+                <li className={animate + " text-2xl p-5 fast"}><a href="#">Experience</a></li>
+                <li className={animate + " text-2xl p-5 fast"}><a href="#">Contact</a></li>
             </ul>
         </nav>);
 }
