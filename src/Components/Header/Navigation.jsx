f9473ea181a5257ab323a2f7e653f91c8fdf7134
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-13 13:28:45 -0400

    Added shadow to the navigation bar. Added an animation while scrolling to the navbar on Desktop view

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -5,8 +5,6 @@ import React, { useState } from "react";
 function Nav(){
     const [menuCollapse, setMenuCollapse] = useState("");
 
-    // const [hamburger, setHamburgerCollapse] = (" ");
-
     const [animate, setAnimation] = useState(() => {
         if (window.innerWidth < 768) {
             return "hidden";
@@ -40,24 +38,73 @@ function Nav(){
             }
         });
     }
-    return (
-        <nav className={"top-0 p-1 fixed bg-gray-900 md:m-auto w-full md:h-auto md:relative z-50 " + menuCollapse}>
-            
-            <div className="inline-block md:hidden pt-4">
-                <button id="ham" onClick={handleMenuCollapse} className="border-gray-900 focus:outline-none ml-2 block hamburger hamburger--collapse-r">
-                    <span className="hamburger-box">
-                        <span className="hamburger-inner"></span>
-                    </span>
-                </button>
-            </div>
 
-            <ul className={elementHidden + " md:flex w-screen md:w-auto justify-center"}>
-                <li className={animate + " text-2xl p-5 fast"}><a className="nav__item" href="#">Home</a></li>
-                <li className={animate + " text-2xl p-5 fast"}><a className="nav__item" href="#">About</a></li>
-                <li className={animate + " text-2xl p-5 fast"}><a className="nav__item" href="#">Work</a></li>
-                <li className={animate + " text-2xl p-5 fast"}><a className="nav__item" href="#">Experience</a></li>
-                <li className={animate + " text-2xl p-5 fast"}><a className="nav__item" href="#">Contact</a></li>
-            </ul>
-        </nav>);
+    window.onscroll = () => {
+      let last_known_scroll_position = window.scrollY;
+
+      if (last_known_scroll_position > 0){
+        document.getElementById("navBar").classList.add("nav--inset");
+        document.getElementById("navBar").classList.remove("ud-nav");
+        
+      }
+      else{
+        document.getElementById("navBar").classList.remove("nav--inset");
+        document.getElementById("navBar").classList.add("ud-nav");
+      }
+    }
+
+    return (
+      <nav
+        id="navBar"
+        className={
+          "shadow-2xl ud-nav top-0 right-0 p-1 fixed bg-gray-900 w-full md:h-auto z-50 " +
+          menuCollapse
+        }
+      >
+        <div className="inline-block md:hidden pt-4">
+          <button
+            id="ham"
+            onClick={handleMenuCollapse}
+            className="border-gray-900 focus:outline-none ml-2 block hamburger hamburger--collapse-r"
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
+
+        <ul
+          className={
+            elementHidden + " md:flex w-screen md:w-auto justify-center"
+          }
+        >
+          <li className={animate + " text-2xl p-5 fast"}>
+            <a className="nav__item" href="#">
+              Home
+            </a>
+          </li>
+          <li className={animate + " text-2xl p-5 fast"}>
+            <a className="nav__item" href="#">
+              About
+            </a>
+          </li>
+          <li className={animate + " text-2xl p-5 fast"}>
+            <a className="nav__item" href="#">
+              Work
+            </a>
+          </li>
+          <li className={animate + " text-2xl p-5 fast"}>
+            <a className="nav__item" href="#">
+              Experience
+            </a>
+          </li>
+          <li className={animate + " text-2xl p-5 fast"}>
+            <a className="nav__item" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
 }
 export default Nav;
--- src/Components/Header/NewPresentation.jsx
+++ src/Components/Header/NewPresentation.jsx
@@ -11,28 +11,28 @@ function Presentation(){
     let be = ["developer", "student"];
 
     return (
-      <div className="flex w-full headerContainer mx-auto mt-10 md:mt-2 md:max-w-6xl items-center">
-        <div className="p-5 text-2xl text-center">
+      <div 
+        className=
+          "flex w-full headerContainer mx-auto md:max-w-6xl items-center fadeInUp animated fast visible"
+      >
+        <div className="p-5 text-2xl text-center fadeInDown">
           <p className="">Hi, my name is</p>
           <h1 className="text-4xl md:text-6xl">Hector Morales</h1>
           <div className="mb-5 flex max-w-sm mx-auto items-center">
-            <p className="w-1/2 text-right mr-2">I'm a </p>
+            <p className="w-1/2 text-right mr-1">I'm a </p>
             <Slider
               className="text-left w-2/3"
               autoplaySpeed={3000}
-              silideToShow={1}
-              slideToScroll={1}
               infinite={true}
               dots={false}
               arrows={false}
               autoplay
               vertical={true}
-              focusOnSelect={false}
               draggable={false}
               touchMove={false}
             >
               {be.map(element => {
-                return <p>{element}</p>;
+                return <p className="text-yellow-500">{element}</p>;
               })}
             </Slider>
           </div>
