ad5b582e33b32f9f0e3cbcf0e0f3e99527f24c6d
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-17 14:55:35 -0400

    Change animation time.
    Change the duration for the text sliding in the prestation.

--- src/Components/pages/Home/Header/NewPresentation.jsx
+++ src/Components/pages/Home/Header/NewPresentation.jsx
@@ -14,11 +14,11 @@ function Presentation(){
       <div className="p-3 presentationContainer shadow-2xl">
         <div className="flex headerContainer mx-auto items-center max-w-6xl">
           <div className="p-5 text-2xl text-center fadeInDown">
-            <p data-aos="fade-in" data-aos-duration="600" className="">Hi</p>
-            <p data-aos="fade-in" data-aos-delay="1000" data-aos-duration="600">My name is</p>
-            <h1 data-aos="fade-right" data-aos-delay="1500" data-aos-duration="600" className="text-4xl md:text-6xl">Hector Morales</h1>
+            <p data-aos="fade-in" data-aos-duration="1000" className="">Hi</p>
+            <p data-aos="fade-in" data-aos-delay="1000" data-aos-duration="1500">My name is</p>
+            <h1 data-aos="fade-right" data-aos-delay="1500" data-aos-duration="2000" className="text-4xl md:text-6xl">Hector Morales</h1>
             <div className="mb-5 flex max-w-sm mx-auto items-center">
-              <p data-aos="fade-right" data-aos-delay="2000" data-aos-duration="600" className="w-1/2 text-right mr-1">I'm a </p>
+              <p data-aos="fade-right" data-aos-delay="2000" data-aos-duration="2000" className="w-1/2 text-right mr-1">I'm a </p>
               <Slider 
                 className="text-left w-2/3"
                 autoplaySpeed={3000}
@@ -30,11 +30,11 @@ function Presentation(){
                 draggable={false}
                 touchMove={false}>
                 {be.map(element => {
-                  return <p data-aos="fade-up" data-aos-delay="2500" data-aos-duration="600" className="text-yellow-500">{element}</p>;
+                  return <p data-aos="fade-up" data-aos-delay="3000" data-aos-duration="3000" className="text-yellow-500">{element}</p>;
                 })}
               </Slider>
             </div>
-            <p data-aos="fade-up" data-aos-delay="3000" data-aos-duration="600">
+            <p data-aos="fade-up" data-aos-delay="3000" data-aos-duration="3000">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dicta
               nulla, dignissimos odio quo qui nisi explicabo maiores fugiat vero
               sit et tempore, quidem quas iure quae autem veritatis esse?
