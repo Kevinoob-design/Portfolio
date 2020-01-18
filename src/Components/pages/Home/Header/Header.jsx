c412b6da9dc9ec2d64298b06ce9e90b1d971a2c4
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-17 23:29:46 -0400

    Very Small Change
    Change animation direcction for the Arrow.

--- src/Components/pages/Home/Header/Header.jsx
+++ src/Components/pages/Home/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header(){
     return (
       <div className="text-white presentationContainer shadow-2xl p-3 items-center text-center flex flex-col justify-center">
         <NewPresentation />
-        <a href="#testimonials" data-aos="fade-in" data-aos-delay="3000" data-aos-duration="3000" data-aos-anchor-placement="top"><PlayForWorkIcon style={{ fontSize: 50 }} /></a>
+        <a href="#testimonials" data-aos="fade-down" data-aos-delay="3000" data-aos-duration="3000" data-aos-anchor-placement="top"><PlayForWorkIcon style={{ fontSize: 50 }} /></a>
       </div>
     );
 }
