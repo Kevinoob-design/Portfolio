f4310c6fbf200a5c107162835be6ba3937b2a20a
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-19 16:39:07 -0400

    Added zoom on the gallery!
    Also, fix small bug with Nav Bar on previous commit.

--- src/Components/pages/Photography/PhotoConatiner.jsx
+++ src/Components/pages/Photography/PhotoConatiner.jsx
@@ -3,9 +3,9 @@ import LazyLoad from 'react-lazy-load';
 
 const PhotoContainer = (props) => {
     return (
-        <div className="my-2 shadow-6xl border-2 rounded" onClick={() => props.click(props.index)} data-aos="fade-up" data-aos-duration="1500" data-aos-anchor-placement="top-bottom" id="PhotoContainer" >
+        <div className="my-2 shadow-6xl border-2 rounded overflow-hidden " onClick={() => props.click(props.index)} data-aos="fade-up" data-aos-duration="1500" data-aos-anchor-placement="top-bottom"  >
             <LazyLoad offsetTop={200}> 
-                <img className="" src={props.params.src} alt={props.params.alt} />
+                <img id="PhotoContainer" className="" src={props.params.src} alt={props.params.alt} />
             </LazyLoad>
         </div>
      );
