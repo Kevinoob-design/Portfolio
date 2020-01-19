99be4b19f31be4e034f4e7a1f7f7fa4c8fcf2684
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-01-19 02:44:52 -0400

    Small change to gallery.

--- src/Components/pages/Photography/PhotoConatiner.jsx
+++ src/Components/pages/Photography/PhotoConatiner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PhotoContainer = (props) => {
     return (
-        <div className="my-2 shadow-6xl border-2 rounded" onClick={props.onClick.onClick} data-aos="fade-in" data-aos-duration="2000" id="PhotoContainer" >
+        <div className="my-2 shadow-6xl border-2 rounded" onClick={props.onClick.onClick} data-aos="fade-up" data-aos-duration="1500" data-aos-anchor-placement="top-bottom" id="PhotoContainer" >
             <img className="align-top" src={props.params} alt={props.params.alt} />
         </div>
      );
--- src/Components/pages/Photography/ShowCase.jsx
+++ src/Components/pages/Photography/ShowCase.jsx
@@ -20,24 +20,25 @@ const ShowCase = () => {
     };
 
     return ( 
-        <div id="Galery" className="px-1 pt-12 md:p-32 text-center align-top shadow-2xl">
-
-            {photos.map((element, index) => { return <PhotoContainer params={element} onClick={openLightbox} />  } )}
-
-            <ModalGateway>
-                {viewerIsOpen ? (
-                    <Modal onClose={closeLightbox}>
-                        <Carousel
-                            currentIndex={currentImage}
-                            views={photos.map(x => ({
-                                ...x,
-                                srcset: x.srcSet,
-                                caption: x.title
-                            }))}
-                        />
-                    </Modal>
-                ) : null}
-            </ModalGateway>
+        <div className="text-center shadow-2xl mx-3 md:mx-40">
+            <div id="Galery" className="px-1 pt-12 md:p-32">
+                    {photos.map((element, index) => { return <PhotoContainer params={element} onClick={openLightbox} />  } )}
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                        <Modal onClose={closeLightbox}>
+                            <Carousel
+                                currentIndex={currentImage}
+                                views={photos.map(x => ({
+                                    ...x,
+                                    srcset: x.srcSet,
+                                    caption: x.title
+                                }))}
+                            />
+                        </Modal>
+                    ) : null}
+                </ModalGateway>
+            </div>
+            <div className="text-2xl items-center pb-12 underline"><p>Shoot with my phone</p></div>
         </div>
      );
 }
