3618863d187aa452308824ac34a160e108be36cf
Hector <Hector_0.2@icloud.com>
2020-01-25 12:21:21 -0400

    Visual change About Card

--- src/Components/pages/Home/BodyComponents/AboutCardComponent.jsx
+++ src/Components/pages/Home/BodyComponents/AboutCardComponent.jsx
@@ -5,11 +5,11 @@ import {Link} from "react-router-dom";
 
 function AboutCard(props){
     return (
-        <Link className={"aboutCard relative w-1/5 flex-grow inline-block md:mx-1 md:my-5 my-2 bg-gray-900 p-2 rounded shadow-xl border-t-4 " + props.params.color} to={props.params.link}>
-            <div data-aos={props.params.animation} data-aos-duration="600">
-                <div className=""><p className="text-2xl text-center">{props.params.title}</p></div>
-                <div className=""><img src={props.params.img.url} alt={props.params.img.alt}/></div>
-                <div className=""><p className="">{props.params.description}</p></div>
+        <Link className="aboutCard relative w-1/5 flex-grow md:mx-1 md:my-5 my-2 bg-gray-900 p-2" to={props.params.link}>
+            <div className={"h-full rounded shadow-xl border-t-4 " + props.params.color} data-aos={props.params.animation} data-aos-duration="600">
+                <div><p className="text-2xl text-center">{props.params.title}</p></div>
+                <div><img src={props.params.img.url} alt={props.params.img.alt}/></div>
+                <div><p className="">{props.params.description}</p></div>
             </div>
         </Link>
     );
