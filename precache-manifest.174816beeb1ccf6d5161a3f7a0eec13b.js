3f37c42b835034e0268cb49fa5d61d72c510e898
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-10-24 00:53:41 -0400

    Updates

---
+++ precache-manifest.174816beeb1ccf6d5161a3f7a0eec13b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66542731aa039069557f984a62eaf86",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1870a7b5d5e3bd08faa6",
+    "url": "/portfolio/static/css/2.4ace2b82.chunk.css"
+  },
+  {
+    "revision": "1870a7b5d5e3bd08faa6",
+    "url": "/portfolio/static/js/2.c6cfbdab.chunk.js"
+  },
+  {
+    "revision": "f90568c270c034e7f99b53f898f47c1f",
+    "url": "/portfolio/static/js/2.c6cfbdab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ca810703f8eb2b7936c9",
+    "url": "/portfolio/static/js/main.f1abf670.chunk.js"
+  },
+  {
+    "revision": "f5ff1fe24da837620bb5",
+    "url": "/portfolio/static/js/runtime-main.c507fcf5.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
